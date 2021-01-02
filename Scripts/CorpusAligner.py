f69b4709ed16afbcad819ef6e668c4b3c2a0db57
Tsimafei Prakapenka <lybot@tut.by>
2021-01-02 16:54:07 +0300

    Corpus Aligner script updated.

--- Scripts/CorpusAligner.py
+++ Scripts/CorpusAligner.py
@@ -1,28 +1,28 @@
-﻿
-path_to_rus_chunk = r'RusssianNotAligned6.txt'
-path_to_bel_chunk = r'BelarusssianNotAligned6.txt'
-with open(path_to_rus_chunk, 'r', encoding = "utf-8") as rus_file, with open(path_to_bel_chunk, 'r', encoding = "utf-8") as bel_file:
-    russianLines = f.readlines()
-    belarussianLines = f.readlines()
+﻿import os
 
-for index, (ruLine, belLine) in enumerate((zip(russianLines, belarussianLines))):
-    maxLine = max(len(ruLine), len(belLine))
-    minLine = min(len(ruLine), len(belLine))
-    coef = minLine/maxLine
+root_path = os.getcwd()
+path_to_rus_chunk = os.path.abspath(os.path.join(root_path, "..", "Data", "ChunksToAlign", "RussianNotAligned6.txt"))
+path_to_bel_chunk = os.path.abspath(os.path.join(root_path, "..", "Data", "ChunksToAlign", "BelarusianNotAligned6.txt"))
+
+for index, (rus_line, bel_line) in enumerate((zip(open(path_to_rus_chunk, 'r', encoding="utf-8"),
+                                                  open(path_to_bel_chunk, 'r', encoding="utf-8")))):
+    max_line = max(len(rus_line), len(bel_line))
+    min_line = min(len(rus_line), len(bel_line))
+    coef = min_line / max_line
 
     # Border coefficients and their modifications were found empirically
-    border = 0.7341772  
-    if (ruLine.startswith('В настоящее время') and belLine.startswith('Цяпер')): border = 0.69
-    if (minLine<=56 or maxLine<=35): border=0.55
-    if (minLine<=10 or maxLine<=10): border=0.49
+    border = 0.7341772
+    if rus_line.startswith('В настоящее время') and bel_line.startswith('Цяпер'): border = 0.69
+    if min_line <= 56 or max_line <= 35: border = 0.55
+    if min_line <= 10 or max_line <= 10: border = 0.49
 
     # You can skip manually some indexes with correct translations
-    if (index == 543 or index == 57349): continue;
-    
-    if coef < border: 
-        print(f'Coef:{coef}')
-        print(f'Border:{border}')
-        print("Index:" + str(index + 1))
-        print(ruLine)
-        print(belLine)      
+    # if index == 543 or index == 57349: continue
+
+    if coef < border:
+        print(f'Coef: {coef}')
+        print(f'Border: {border}')
+        print(f'Index: {index + 1}')
+        print(rus_line)
+        print(bel_line)
         break
